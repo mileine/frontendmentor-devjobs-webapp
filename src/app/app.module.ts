e2f4cd30a4bde8fae69cfcc585fbe59bdd4a4f99
mileine <mileine.assato@gmail.com>
2021-10-06 22:36:48 -0300

    Add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LayoutComponent } from './shared/layout/layout.component';
 import { HomeComponent } from './views/home/home.component';
 import { JobDetailsPageComponent } from './views/job-details-page/job-details-page.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
     HomeComponent,
-    JobDetailsPageComponent
+    JobDetailsPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
