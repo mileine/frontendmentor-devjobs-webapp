7103f602bc27d22f66dad09b778bd45bdea85c46
mileine <mileine.assato@gmail.com>
2025-11-09 15:08:04 -0300

    remove angular related files

--- src/app/models/requirements.model.ts
+++
@@ -1,8 +0,0 @@
-export class Requirements {
-    public content: string;
-    public items: String[];
-    constructor (content: string, items: String[]) {
-        this.content = content;
-        this.items = items;
-    }
-}
